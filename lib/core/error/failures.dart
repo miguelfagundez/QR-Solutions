65ddc2164e4cccc01b4882a58dc99cc98e5fa2d6
Miguel Fagundez <59711011+miguelfagundez@users.noreply.github.com>
2024-11-17 11:03:08 -0500

    Update failure abstract class

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -7,14 +7,26 @@ abstract class Failure {
     required this.code,
     required this.message,
   });
+
+  void writeError();
 }
 
 class LocalDataBaseFailure extends Failure {
   // Internal info for database errors
   LocalDataBaseFailure({required super.code, required super.message});
+
+  @override
+  void writeError() {
+    // TODO: implement writeError
+  }
 }
 
 class PreferencesFailure extends Failure {
   // Internal info for preferences errors
   PreferencesFailure({required super.code, required super.message});
+
+  @override
+  void writeError() {
+    // TODO: implement writeError
+  }
 }
